f9fcc607c4dc0b0847bd5ff0dfca47b2c08cef30
Helge Skogly Holm <helge.holm@gmail.com>
2014-05-23 23:30:34 +0200

    No need to test browser-minified version in nodejs context.

--- test/randy.spec.js
+++ test/randy.spec.js
@@ -2,11 +2,7 @@
 
 if (typeof randy == "undefined") {
     // Predefined in browser mode, so assume nodejs context.
-    if (process.env.TEST_MINIFIED_VERSION == 'yes') {
-        randy = require("../lib/randy.min");
-    } else {
-        randy = require("../lib/randy");
-    }
+    var randy = require(__dirname + "/../lib/randy");
     var assert = require("assert");
 }
 
